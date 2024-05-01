3ad80509d0d333df58a2649d192d19d74b089412
wherna48 <129709943+wherna48@users.noreply.github.com>
2024-04-30 22:39:03 -0400

    Add files via upload

---
+++ chatRoom lab/ChatClient.java
@@ -0,0 +1,135 @@
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+//import javafx.stage.Stage;
+import javafx.stage.Stage;
+
+import javax.swing.JOptionPane;
+
+
+public class ChatClient implements Runnable, ChatClientInterface {
+	private static int CHAT_ROOM_PORT = 0;
+	boolean local =true;
+    BufferedReader in;
+    PrintWriter out = null;
+    BorderPane frame = new BorderPane();
+    TextField textField = new TextField();
+    TextArea messageArea = new TextArea();
+    Stage stage;
+    String myScreenName = "";
+    
+	ChatClient(int port) {
+		CHAT_ROOM_PORT = port;
+
+        // Layout GUI
+		stage = new Stage();
+		stage.setScene(new Scene(frame, 500, 200));
+		stage.setX(ChatClientExec.getClientX());
+		stage.setY(ChatClientExec.getClientY());
+		// Set stage title and show the stage.
+		stage.setTitle("Chat Client");
+		stage.show();
+        textField.setEditable(false);
+        messageArea.setEditable(false);
+        frame.setTop(textField); 
+        frame.setCenter(messageArea); 
+        frame.setVisible(true);
+        
+        /**
+         * Responds to pressing the enter key in the textfield by sending
+         * the contents of the text field to the server.    Then clear
+         * the text area in preparation for the next message.
+         */
+        textField.setOnAction(event -> {
+        			try {
+        				out.println(textField.getText());
+                        textField.setText("");
+                        out.flush();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+        });
+	}
+
+	/**
+     * Prompt for and return the address of the server.
+     */
+    private String getServerAddress() {
+    	return "localHost";
+    }
+
+    /**
+     * Prompt for and return the desired screen name.
+     */
+    public String getName() {
+        return JOptionPane.showInputDialog(
+            null,
+            "Choose a screen name:",
+            "Screen name selection",
+            JOptionPane.PLAIN_MESSAGE);
+    }
+
+    /**
+     * Connects to the server then enters the processing loop.
+     */
+    public void run() {
+		try {
+			//TODO STUDENT: create a client socket with server address and server port
+			Socket clientSocket = new Socket(getServerAddress(), getServerPort());
+			
+			//Make connection and initialize streams;
+			//TODO STUDENT: setup input and output streams
+			
+			InputStream inStream = clientSocket.getInputStream();
+			OutputStream outStream = clientSocket.getOutputStream();
+			
+			out = new PrintWriter(outStream);
+			Reader readIn = new InputStreamReader(inStream);
+			in = new BufferedReader(readIn);
+			
+	        // Process all messages from server, according to the protocol.
+	        while (true) {
+	            String line = in.readLine();
+	            if (line.startsWith("SUBMITNAME")) {
+	            	System.out.println("client received SUBMITNAME");
+	            	myScreenName = getName();
+	                out.println(myScreenName);
+	                out.flush();
+	                
+	            } else if (line.startsWith("NAMEACCEPTED")) {
+	            	System.out.println("client received NAMEACCEPTED");
+	                textField.setEditable(true);
+	            } else if (line.startsWith("WRONGNAME")) {
+	            	System.out.println("client received WRONGNAME");
+	            	JOptionPane.showMessageDialog(null, "Screen Name "+myScreenName+" is already in use.");
+	            } else if (line.startsWith("MESSAGE")) {
+	            	System.out.println("client received MESSAGE");
+	            	//MESSAGE " + name + ": " + input
+	                messageArea.appendText(line.substring(8) + "\n");
+	            }
+	        }
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+
+    public int getServerPort() {
+		return CHAT_ROOM_PORT;
+	}
+}
